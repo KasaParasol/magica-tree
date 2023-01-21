7fe50e1cd337ec251b8c2c252c19220bc3051614
KasaParasol <yanase.takuma.oct+tsukasa@gmail.com>
2023-01-21 21:01:12 +0900

    first commit

---
+++ readme.md
@@ -0,0 +1,28 @@
+![MagicaTree](attachment/logo.png)
+===
+
+MagicaTree は リスト (`ul`、`li`) を元にドラッグアンドドロップで操作できるツリーUIを構築します。
+
+![サンプル](attachment/sample.gif)
+
+使用方法
+---
+
+MagicaTree をインストールするコマンドを以下に記...
+そうと思いましたが、`npm`に`publish`したら書きます。
+
+### ライブラリ利用方法 ###
+
+
+以下サンプル
+
+```javascript
+```
+
+ドキュメント
+---
+
+他
+---
+
+- `logo.png` 背景素材はぱくたそ様より
---
+++ webpack.config.cjs
@@ -0,0 +1,15 @@
+const path = require('node:path');
+
+module.exports = env => ({
+    entry: './src/index.js',
+    output: {
+        path: env.mode === 'demo' ? path.resolve('./docs') : path.resolve('./dist'),
+        filename: 'main.js',
+        library: {
+            name: 'MagicaTree',
+            type: 'umd',
+        },
+    },
+    mode: 'development',
+    devtool: 'inline-source-map',
+});
